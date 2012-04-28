e0618eb6b177c9890a36b3b2426b6899e72ac817
Philippe Guilbault <pguilbault@funio.com>
2012-04-28 14:29:38 -0400

    Added template and i18n tools

---
+++ NoobTemplates.js
@@ -0,0 +1,33 @@
+var templateFiles = {}, loadingFiles = {};
+
+function NoobTemplates() {
+    "use strict";
+}
+
+NoobTemplates.prototype.get = function get(filename, callback) {
+    "use strict";
+
+    var self = this, file = filename;
+    if (templateFiles.hasOwnProperty(filename)) {
+        callback(undefined, templateFiles[filename]);
+    } else if (localStorage !== undefined && localStorage.hasOwnProperty('templates:' + filename)) {
+        templateFiles[filename] = localStorage['templates:' + filename];
+        callback(undefined, templateFiles[filename]);
+    } else if (!loadingFiles.hasOwnProperty(filename)) {
+        loadingFiles[filename] = true;
+        socketio.static_files.emit('fetch', file);
+        socketio.static_files.on(file, function socketioFile(err, html) {
+            if (err) {
+                callback(err, undefined);
+                return;
+            }
+
+            // localStorage[ 'templates:'+filename ] = html;
+            templateFiles[filename] = html;
+            callback(undefined, templateFiles[filename]);
+            delete loadingFiles[filename];
+        });
+    }
+};
+
+noobTemplates = new NoobTemplates();
